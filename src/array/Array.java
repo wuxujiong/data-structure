982fd2fad27245efac2d62732162f059e0e75e3a
david <1786480347@qq.com>
2018-10-29 19:40:31 +0800

    Array extend

--- src/array/Array.java
+++ src/array/Array.java
@@ -3,9 +3,9 @@ package array;
 /**
  * Created by DELL on 2018/10/12.
  */
-public class Array {
+public class Array <E>{
 
-    private int[] data;
+    private E[] data;
     private int size;
 
     /**
@@ -13,7 +13,7 @@ public class Array {
      * @param capacity
      */
     public Array(int capacity){
-        data = new int[capacity];
+        data = (E[])new Object[capacity];
         size=0;
     }
     //无参数的构造函数
@@ -33,21 +33,23 @@ public class Array {
         return size==0;
     }
 
-    public void addLast(int e){
+    public void addLast(E e){
        add(size,e);
     }
 
-    public void addFirst(int e){
+    public void addFirst(E e){
         add(0,e);
     }
 
 
-    public void add(int index,int e){
-        if (size==data.length){
-            throw new IllegalArgumentException("Add failed.Array is fulll.");
-        }
+    public void add(int index,E e){
+
         if (index<0 || index>size){
             throw new IllegalArgumentException("Add failed.Require index<0 || index>size.");
+        }
+
+        if (size==data.length){
+           resize(2 * data.length);
         }
 
         for (int i=size-1;i>=index;i--){
@@ -60,14 +62,14 @@ public class Array {
     }
 
 
-    public int get(int index){
+    public E get(int index){
         if (index<0 || index>=size){
             throw new IllegalArgumentException("get failed.Index is illegal");
         }
         return data[index];
     }
 
-    public void set(int index,int e){
+    public void set(int index,E e){
 
         if (index<0 || index>=size){
             throw new IllegalArgumentException("get failed.Index is illegal");
@@ -76,19 +78,55 @@ public class Array {
 
     }
 
-    public boolean contains(int e){
+    public boolean contains(E e){
         for (int i=0;i<size;i++){
-            if (data[i]==e){
+            if (data[i].equals(e)){
                 return true;
             }
         }
         return false;
     }
 
-    public  int find(int e){
+    public E remove(int index){
+        if (index<0 || index>=size){
+            throw new IllegalArgumentException("get failed.Index is illegal");
+        }
+
+        E ret = data[index];
+
+        for (int i=index+1;i<size;i++){
+            data[i-1]=data[i];
+        }
+        size--; // loitering objects != memory leak
+
+        if (size == data.length /4 && data.length /2 !=0){
+            resize(data.length/2);
+        }
+        return ret;
+    }
+
+
+    public E removeFirst(){
+        return  remove(0);
+
+    }
+
+    public E removeLast(){
+        return remove(size-1);
+    }
+
+
+    public void removeElement(E e){
+        int index=  find(e);
+        if (index!=-1){
+            remove(index);
+        }
+    }
+
+    public  int find(E e){
 
         for (int i=0;i<size;i++){
-            if (data[i]==e){
+            if (data[i].equals(e)){
                return i;
             }
         }
@@ -114,5 +152,13 @@ public class Array {
         }
         res.append("]");
         return res.toString();
+    }
+
+    private void resize(int newCapacity){
+        E[] newData = (E[])new Object[newCapacity];
+        for (int i=0;i<size;i++){
+            newData[i]=data[i];
+        }
+        data = newData;
     }
 }
--- src/array/Main.java
+++ src/array/Main.java
@@ -6,7 +6,7 @@ package array;
 public class Main {
 
     public static void main(String[] args) {
-        Array arr = new Array(20);
+        Array<Integer> arr = new Array<>();
         for (int i=0;i<10;i++){
             arr.addLast(i);
         }
@@ -18,5 +18,8 @@ public class Main {
 
         arr.addFirst(-1);
         System.out.println(arr);
+
+//        arr.remove(2);
+//        System.out.println(arr);
     }
 }
